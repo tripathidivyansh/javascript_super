9f6a856f811ae9791e0eb6b6d859aae721c112ed
tripathidivyansh <tripatidivyansh144@gmail.com>
2025-04-29 12:56:08 +0530

    oppS

---
+++ Opps/opps.js
@@ -0,0 +1,23 @@
+// javaScript and classes
+
+// oopS
+
+// object
+
+// -collection of properties and methods
+
+// toLowerCase
+
+// why use oppS
+// object literal
+
+// constructor function
+// prototypes
+// classes
+// inStance (new,this)
+
+// 4 pillars
+// abstraction
+// encapsulation
+// inheritance
+// polymorphism
---
+++ Opps/opps_one.js
@@ -0,0 +1,40 @@
+const user = {
+    username: "malik",
+    logicCount: 8,
+    signedIn: true,
+    getuserDetails: function(){
+        // // console.log("Got user detauls from database");
+        // console.log(`username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getuserDetails());
+
+// console.log(this);
+
+
+const promise = new Promise()
+const date = new Date();
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`);
+    }
+    return this
+}
+
+const userone = new User("malik", 12, true);
+const usertwo = new User("chaiaurcode", 11, false);
+
+console.log(userone);
+console.log(usertwo);
+ console.log(userone.constructor);
+ 
+ 
